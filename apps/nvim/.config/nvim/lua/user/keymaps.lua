a52704a9ba306c8aaeb5c77cec9cd66d84b0c6c0
Devon Walker <devwalker@tesla.com>
2023-07-06 14:16:17 -0500

    Remove escape delay

--- apps/nvim/.config/nvim/lua/user/keymaps.lua
+++ apps/nvim/.config/nvim/lua/user/keymaps.lua
@@ -3,7 +3,7 @@ local opts = { noremap = true, silent = true }
 local term_opts = { silent = true }
 
 -- Leader key
-keymap("", "<Space>", "<Nop>", opts)
+--vim.keymap.set("", "<Space>", "<Nop>", opts)
 vim.g.mapleader = " "
 vim.g.maplocalleader = " "
 
@@ -21,53 +21,54 @@ vim.api.nvim_create_user_command('Reload', ':luafile $MYVIMRC', {})
 
 -----------------
 -- Normal
-keymap("n", "<leader>w", ":w<CR>", opts) -- save
-keymap("n", "<C-w>", ":bd<CR>", opts) -- close buffer
-keymap("n", "<leader>q", ":q<CR>", opts) -- quit
-keymap("n", "<leader>n", ":nohl<CR>", opts) -- clear highlights after search
-keymap("n", "<leader>z", ":ZenMode<CR>", opts) -- ZenMode
-keymap("n", "<leader>r", ":Reload<CR>", opts) -- Reload config
-keymap("n", "<leader>t", ":ToggleTerm<CR>", opts) -- clear highlights after search
+vim.keymap.set("n", "<leader>w", ":w<CR>", opts) -- save
+vim.keymap.set("n", "<C-w>", ":bd<CR>", opts) -- close buffer
+--vim.keymap.set("n", "<leader>q", ":q<CR>", opts) -- quit
+vim.keymap.set("n", "<leader>q", "<cmd>q<cr>", opts) -- quit
+vim.keymap.set("n", "<leader>n", ":nohl<CR>", opts) -- clear highlights after search
+vim.keymap.set("n", "<leader>z", ":ZenMode<CR>", opts) -- ZenMode
+vim.keymap.set("n", "<leader>r", ":Reload<CR>", opts) -- Reload config
+vim.keymap.set("n", "<leader>t", ":ToggleTerm<CR>", opts) -- clear highlights after search
 
 -- Sidebars
-keymap("", "<Space>", "<Nop>", opts)
-keymap("n", "<C-e>", ":NvimTreeToggle 30<CR>", opts) -- explorer
+--vim.keymap.set("", "<Space>", "<Nop>", opts)
+vim.keymap.set("n", "<C-e>", ":NvimTreeToggle 30<CR>", opts) -- explorer
 -- debug
 -- testing
 -- git
 
 -- Resize with arrows
-keymap("n", "<C-Up>", ":resize +2<CR>", opts)
-keymap("n", "<C-Down>", ":resize -2<CR>", opts)
-keymap("n", "<C-Left>", ":vertical resize -2<CR>", opts)
-keymap("n", "<C-Right>", ":vertical resize +2<CR>", opts)
+vim.keymap.set("n", "<C-Up>", ":resize +2<CR>", opts)
+vim.keymap.set("n", "<C-Down>", ":resize -2<CR>", opts)
+vim.keymap.set("n", "<C-Left>", ":vertical resize -2<CR>", opts)
+vim.keymap.set("n", "<C-Right>", ":vertical resize +2<CR>", opts)
 
 -- Navigate buffers
-keymap("n", "<C-Tab>", ":bnext<CR>", opts)
-keymap("n", "<C-S-Tab>", ":bprevious<CR>", opts)
-keymap("n", "<S-l>", ":bnext<CR>", opts)
-keymap("n", "<S-h>", ":bprevious<CR>", opts)
+vim.keymap.set("n", "<C-Tab>", ":bnext<CR>", opts)
+vim.keymap.set("n", "<C-S-Tab>", ":bprevious<CR>", opts)
+vim.keymap.set("n", "<S-l>", ":bnext<CR>", opts)
+vim.keymap.set("n", "<S-h>", ":bprevious<CR>", opts)
 
 -- Neovide zoom
 if vim.g.neovide == true then
-  vim.api.nvim_set_keymap("n", "<C-+>", ":lua vim.g.neovide_scale_factor = vim.g.neovide_scale_factor + 0.1<CR>", { silent = true })
-  vim.api.nvim_set_keymap("n", "<C-->", ":lua vim.g.neovide_scale_factor = vim.g.neovide_scale_factor - 0.1<CR>", { silent = true })
-  vim.api.nvim_set_keymap("n", "<C-0>", ":lua vim.g.neovide_scale_factor = 1<CR>", { silent = true })
+  vim.api.nvim_set_vim.keymap.set("n", "<C-+>", ":lua vim.g.neovide_scale_factor = vim.g.neovide_scale_factor + 0.1<CR>", { silent = true })
+  vim.api.nvim_set_vim.keymap.set("n", "<C-->", ":lua vim.g.neovide_scale_factor = vim.g.neovide_scale_factor - 0.1<CR>", { silent = true })
+  vim.api.nvim_set_vim.keymap.set("n", "<C-0>", ":lua vim.g.neovide_scale_factor = 1<CR>", { silent = true })
 end
 
 -----------------
 -- Visual
 -- Paste replace without overwrite
-keymap("v", "p", '"_dP', opts)
+vim.keymap.set("v", "p", '"_dP', opts)
 
 -- Stay in indent mode
-keymap("v", "<", "<gv", opts)
-keymap("v", ">", ">gv", opts)
+vim.keymap.set("v", "<", "<gv", opts)
+vim.keymap.set("v", ">", ">gv", opts)
 
 -- Visual Block
 -- Paste replace without overwrite
-keymap("x", "p", '"_dP', opts)
+vim.keymap.set("x", "p", '"_dP', opts)
 
 -- Move text up and down
-keymap("x", "J", ":move '>+1<CR>gv-gv", opts)
-keymap("x", "K", ":move '<-2<CR>gv-gv", opts)
+vim.keymap.set("x", "J", ":move '>+1<CR>gv-gv", opts)
+vim.keymap.set("x", "K", ":move '<-2<CR>gv-gv", opts)
--- apps/nvim/.config/nvim/lua/user/nvim-tree.lua
+++ apps/nvim/.config/nvim/lua/user/nvim-tree.lua
@@ -10,4 +10,22 @@ vim.g.loaded_netrwPlugin = 1
 -- set termguicolors to enable highlight groups
 vim.opt.termguicolors = true
 
-nvim_tree.setup()
+local function my_on_attach(bufnr)
+  local api = require "nvim-tree.api"
+
+  local function opts(desc)
+    return { desc = "nvim-tree: " .. desc, buffer = bufnr, noremap = true, silent = true, nowait = true }
+  end
+
+  -- default mappings
+  api.config.mappings.default_on_attach(bufnr)
+
+  -- custom mappings
+  -- TODO vim.keymap.set('n', '<C-e>', '<Nop>', {}) currently unsetting c-e outside of nvim-tree buffer
+  vim.keymap.set('n', '?', api.tree.toggle_help, opts('Help'))
+end
+
+-- pass to setup along with your other options
+nvim_tree.setup {
+  on_attach = my_on_attach,
+}
