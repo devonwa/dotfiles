2435f1771251e0bba479f62fe7608f76b679e7de
Devon Walker <devwalker@tesla.com>
2024-03-20 14:55:14 -0700

    update wezterm for mac

--- apps/wezterm/.config/wezterm/wezterm.lua
+++ apps/wezterm/.config/wezterm/wezterm.lua
@@ -4,15 +4,26 @@ if wezterm.config_builder then
   config = wezterm.config_builder()
 end
 
+-- config.color_scheme = 'Catppuccin Macchiato'
+-- config.color_scheme = '3024 (dark) (terminal.sexy)'
+-- config.color_scheme = '3024 Night (Gogh)'
+-- config.color_scheme = 'Afterglow'
+-- config.color_scheme = 'GitHub Dark'
+
+config.font_size = 14.0
+config.font = wezterm.font('FiraCode Nerd Font', { weight = 'Medium' })
+
 config.check_for_updates = false
-config.color_scheme = 'Catppuccin Macchiato'
 config.enable_tab_bar = false
 
+config.window_background_opacity = 0.90
 config.window_padding = {
   left = 0,
   right = 0,
   top = 0,
   bottom = 0,
 }
+
+config.scrollback_lines = 50000
 
 return config
