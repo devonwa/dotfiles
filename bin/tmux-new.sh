be9971f67a7c1a6c860cfc642285faa5ba52da9d
Devon Walker <devwalker@tesla.com>
2024-10-17 16:48:09 -0700

    swap tmux windows

--- bin/tmux-new.sh
+++ bin/tmux-new.sh
@@ -4,11 +4,11 @@ SESSION=$(basename $(pwd))
 
 tmux new-session -d -s $SESSION
 
-tmux rename-window -t 1 'git'
-tmux new-window -t $SESSION:2 -n 'vim'
+tmux rename-window -t 1 'vim'
+tmux new-window -t $SESSION:2 -n 'git'
 tmux new-window -t $SESSION:3
 
-tmux send-keys -t 'git' 'lg' C-m
 tmux send-keys -t 'vim' 'nvim' C-m
+tmux send-keys -t 'git' 'lg' C-m
 
 tmux attach-session -t $SESSION:3
