3f0944bfe7e49e6a6abb01366f02d0b148941635
Devon Walker <devwalker@tesla.com>
2024-11-05 11:01:31 -0800

    update tmux

--- bin/tmux-new.sh
+++ bin/tmux-new.sh
@@ -8,6 +8,12 @@ tmux rename-window -t 1 'vim'
 tmux new-window -t $SESSION:2 -n 'git'
 tmux new-window -t $SESSION:3
 
+tmux split-window -h
+tmux split-window -v
+tmux select-pane -t 2 # moving right highlights top window first
+tmux select-pane -t 1
+
+tmux send-keys -t 'vim' 'nvim' C-m
 tmux send-keys -t 'vim' 'nvim' C-m
 tmux send-keys -t 'git' 'lg' C-m
 
