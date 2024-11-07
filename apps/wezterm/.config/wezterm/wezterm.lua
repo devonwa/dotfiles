ee6c005de035c4e72c9d9950211e0f27b9532804
Devon Walker <devwalker@tesla.com>
2024-11-07 10:22:56 -0800

    wezterm reformat

--- apps/wezterm/.config/wezterm/wezterm.lua
+++ apps/wezterm/.config/wezterm/wezterm.lua
@@ -4,17 +4,18 @@ if wezterm.config_builder then
   config = wezterm.config_builder()
 end
 
+config.font_size = 14.0
+config.font = wezterm.font('FiraCode Nerd Font', { weight = 'Medium' })
+
 -- config.color_scheme = 'Catppuccin Macchiato'
 -- config.color_scheme = '3024 (dark) (terminal.sexy)'
 -- config.color_scheme = '3024 Night (Gogh)'
 -- config.color_scheme = 'Afterglow'
 -- config.color_scheme = 'GitHub Dark'
 
-config.font_size = 14.0
-config.font = wezterm.font('FiraCode Nerd Font', { weight = 'Medium' })
-
 config.check_for_updates = false
 config.enable_tab_bar = false
+config.scrollback_lines = 50000
 
 config.window_background_opacity = 0.90
 config.window_padding = {
@@ -23,7 +24,5 @@ config.window_padding = {
   top = 0,
   bottom = 0,
 }
-
-config.scrollback_lines = 50000
 
 return config
