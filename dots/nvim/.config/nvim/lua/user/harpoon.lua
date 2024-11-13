2f46aaeb175e72f64d1a952b132d3aba9a64cb26
Devon Walker <devwalker@tesla.com>
2024-11-13 14:59:17 -0800

    fixup harpoon and treesitter

--- dots/nvim/.config/nvim/lua/user/harpoon.lua
+++ dots/nvim/.config/nvim/lua/user/harpoon.lua
@@ -8,6 +8,13 @@ vim.keymap.set("n", "<leader>1", function() harpoon:list():select(1) end)
 vim.keymap.set("n", "<leader>2", function() harpoon:list():select(2) end)
 vim.keymap.set("n", "<leader>3", function() harpoon:list():select(3) end)
 vim.keymap.set("n", "<leader>4", function() harpoon:list():select(4) end)
+vim.keymap.set("n", "<leader>5", function() harpoon:list():select(5) end)
+-- not working: terminal eating ctrl+number ?
+-- vim.keymap.set("n", "<C-1>", function() harpoon:list():select(1) end)
+-- vim.keymap.set("n", "<C-2>", function() harpoon:list():select(2) end)
+-- vim.keymap.set("n", "<C-3>", function() harpoon:list():select(3) end)
+-- vim.keymap.set("n", "<C-4>", function() harpoon:list():select(4) end)
+-- vim.keymap.set("n", "<C-5>", function() harpoon:list():select(5) end)
 
 -- Toggle previous & next buffers stored within Harpoon list
 vim.keymap.set("n", "<leader>h", function() harpoon:list():prev() end)
