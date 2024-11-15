9d6ef2e0ed45380392ff341ea9c1edfbb1dff84b
Devon Walker <devwalker@tesla.com>
2024-11-14 16:43:02 -0800

    add neovide settings

---
+++ dots/nvim/.config/nvim/lua/user/neovide.lua
@@ -0,0 +1,9 @@
+-- https://neovide.dev/configuration.html
+if vim.g.neovide then
+    vim.g.neovide_cursor_animate_command_line = true
+    vim.g.neovide_cursor_animation_length = 0.05
+    vim.g.neovide_cursor_trail_size = 0.8
+    vim.g.neovide_position_animation_length = 0.15
+    vim.g.neovide_scroll_animation_length = 0.0
+    vim.g.neovide_scroll_animation_far_lines = 0
+end
