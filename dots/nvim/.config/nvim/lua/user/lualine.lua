a97c470da36ed34623b207324f9325ea89ddfc25
Devon Walker <devwalker@tesla.com>
2025-11-13 17:20:29 -0800

    update lualine

--- dots/nvim/.config/nvim/lua/user/lualine.lua
+++ dots/nvim/.config/nvim/lua/user/lualine.lua
@@ -1,1 +1,18 @@
-require('lualine').setup()
+local function get_git_root_name()
+    local git_dir = vim.fn.system('git rev-parse --show-toplevel 2>/dev/null')
+    git_dir = vim.trim(git_dir)
+    if git_dir ~= '' and vim.v.shell_error == 0 then
+        return vim.fn.fnamemodify(git_dir, ':t')
+    end
+    return ''
+end
+
+require('lualine').setup({
+    options = { theme = 'gruvbox' },
+    sections = {
+        lualine_a = { get_git_root_name },
+        lualine_x = { 'encoding', 'fileformat', 'filetype' },
+        lualine_y = { 'lsp_status' },
+        lualine_z = { 'location' }
+    },
+})
